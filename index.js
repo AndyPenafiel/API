f6e4b56a55d9cf681f2d660b9ac177c9fab2eab4
Andy <andypenafielb2@gmail.com>
2025-02-18 23:28:06 -0500

    segundo

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import pg from 'pg';
+
+// Cargar variables de entorno
+dotenv.config();
 
 // Crear una nueva aplicación Express
 const app = express();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Asegúrate de que este valor sea adecuado para tu configuración
+  }
+});
 
 // Definir un puerto para nuestro servidor
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000; // Usar process.env.PORT si está disponible
 
 // Definir una ruta de prueba
 app.get('/', (req, res) => {
   res.send('¡Hola Mundo!');
+});
+
+app.get('/ping', async (req, res) => {
+  const result = await pool.query('SELECT NOW()');
+  return res.json(result.rows[0]); // Usar 'rows' en lugar de 'row'
 });
 
 // Iniciar el servidor
